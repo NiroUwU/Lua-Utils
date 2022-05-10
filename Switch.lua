70e11ec11bc6de0b12a6f5429e71924d559b88cc
Niro <80123634+NiroUwU@users.noreply.github.com>
2022-05-10 19:36:17 +0200

    added switch/case

---
+++ Switch.lua
@@ -0,0 +1,21 @@
+Switch = {}
+
+function Switch.switch(val, cases)
+	-- Loop through cases and execute, return if found:
+	for i, v in pairs(cases) do
+		if val == i then
+			v()
+			return
+		end
+	end
+
+	-- Check if default case is given:
+	if not cases['default'] then
+		return
+	end
+	
+	-- Run default statement:
+	cases['default']()
+end
+
+return Switch
--- examples/switch-example.lua
+++ examples/switch-example.lua
@@ -1,19 +1,3 @@
-# Lua-Utils
-
-A collection of libraries/modules to ease your coding life with Lua. :)
-
-Feel free to use and or modify the code for your own projects.
-
-## In this collection
-
-### _Switch_
-
-This is an implementation of the Switch/case statement written for Lua, as it
-does not have its own switch/case out of the box.
-
-Usage:
-
-```lua
 require("Switch")             -- import the module
 local switch = Switch.switch  -- optional but recommended for better code readability
 
@@ -35,6 +19,3 @@ switch(name, {
 		print(name .. " wasn't found :(")
 	end
 })
-```
-
-<img src="./.img/switch.jpg" alt="Switch Statement Picture" width=550/> 
